a3cd228f3d7f919e336357a651cf44fe3f787bc6
Rene Dohan <dohan.rene@gmail.com>
2013-02-15 21:17:40 +0100

    no message

---
+++ Core/CSPullToRefresh/CSPullToRefreshView.h
@@ -0,0 +1,14 @@
+//
+// Created by inno on 2/15/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "PullToRefreshView.h"
+
+
+@interface CSPullToRefreshView : PullToRefreshView
+- (void)setTextColor:(UIColor *)color;
+@end
---
+++ Core/CSPullToRefresh/CSPullToRefreshView.m
@@ -0,0 +1,20 @@
+//
+// Created by inno on 2/15/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "CSPullToRefreshView.h"
+
+
+@implementation CSPullToRefreshView {
+
+}
+
+- (void)setTextColor:(UIColor *)color {
+    lastUpdatedLabel.textColor = color;
+    statusLabel.textColor = color;
+}
+
+@end
