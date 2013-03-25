86af217630f283423ddbb3b33dc45b6b1063088e
Rene Dohan <dohan.rene@gmail.com>
2013-03-25 13:43:55 +0100

    no message

---
+++ Core/Types/View/CSAlertView.h
@@ -0,0 +1,15 @@
+//
+// Created by inno on 1/31/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface CSAlertView : NSObject <UIAlertViewDelegate>
+-(CSAlertView *)show:(NSString *)title :(NSString *)message :(NSString *)cancelTitle :(NSString *)okTitle:(void (^)())onSubmit;
+
+- (CSAlertView *)show:(NSString *)title :(NSString *)message :(NSString *)button1 :(void (^)())button1Action :(NSString *)button2 :(void (^)())button2Action;
+@end
