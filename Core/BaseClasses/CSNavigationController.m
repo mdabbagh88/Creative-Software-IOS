63def07c265949255e635be3a6fcb8153cf4afe7
Rene Dohan <dohan.rene@gmail.com>
2013-03-31 16:43:40 +0200

    no message

--- Core/BaseClasses/CSNavigationController.m
+++ Core/BaseClasses/CSNavigationController.m
@@ -16,17 +16,17 @@
 }
 
 - (UIViewController *)popViewControllerAnimated:(BOOL)animated {
-    _popped = self.last;
+    _lastPoped = self.last;
     return [super popViewControllerAnimated:animated];
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    _popped = nil;
+    _lastPoped = nil;
     [super pushViewController:viewController animated:animated];
 }
 
 - (void)pushRootViewController:(UIViewController *)newRoot {
-    _popped = nil;
+    _lastPoped = nil;
     [super pushRootViewController:newRoot];
 }
 
@@ -35,7 +35,7 @@
 }
 
 - (NSArray *)popToViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    _popped = viewController;
+    _lastPoped = viewController;
     return [super popToViewController:viewController animated:animated];
 }
 
